a55b42c1119503538d5909ba5cce795cad2b5f57
Chocohead <Chocohead@Aroma1997.org>
2024-03-15 23:04:14 +0000

    Fix servers not sending stat packets
    Somewhat inevitable given `EntityPlayerSP#addStat` is also a no-op

---
+++ src/server/java/com/chocohead/tfh/mixins/server/EntityPlayerMPMixin.java
@@ -0,0 +1,38 @@
+package com.chocohead.tfh.mixins.server;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.src.game.achievements.Achievement;
+import net.minecraft.src.game.entity.player.EntityPlayer;
+import net.minecraft.src.game.entity.player.EntityPlayerMP;
+import net.minecraft.src.game.stats.StatBase;
+import net.minecraft.src.server.packets.NetServerHandler;
+import net.minecraft.src.server.packets.Packet200Statistic;
+
+@Mixin(EntityPlayerMP.class)
+abstract class EntityPlayerMPMixin extends EntityPlayer {
+	@Shadow
+	public NetServerHandler playerNetServerHandler;
+
+	private EntityPlayerMPMixin() {
+		super(null);
+	}
+
+	@Inject(method = "addStat", at = @At("HEAD"))
+	private void registerStat(StatBase stat, int value, CallbackInfo call) {
+		if (stat == null) return;
+
+		if (!stat.field_27058_g) {
+			while (value > 100) {
+				playerNetServerHandler.sendPacket(new Packet200Statistic(stat.statId, 100));
+				value -= 100;
+			}
+
+			playerNetServerHandler.sendPacket(new Packet200Statistic(stat.statId, value));
+		}
+	}
+}
